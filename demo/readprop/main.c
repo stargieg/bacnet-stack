2439894bad19d9cffd276a9c4c1f48d1d46c3a3e
Bang <OpenLogical@users.noreply.github.com>
2016-04-10 11:27:02 -0500

    Add TCP server and  modula readprop

--- demo/readprop/main.c
+++ demo/readprop/main.c
@@ -204,16 +204,108 @@ int main (int argc, char *argv[]) {
         return 1;
     }*/
 
-
     init_readprop();
-    readprop (40101, 0, 1,85,-1);
-    readprop (50100, 0,1,85,-1);
 
 
-    if (Error_Detected)
+    //STARTING TCP SERVER
+
+    prints ("Starting up TCP server\r\n");
+
+    //A SOCKET is simply a typedef for an unsigned int.
+    SOCKET server;
+
+    //WSADATA is a struct that is filled up by the call to WSAStartup
+    WSADATA wsaData;
+
+    //The sockaddr_in specifies the address of the socket
+    //for TCP/IP sockets. Other protocols use similar structures.
+    sockaddr_in local;
+
+    //WSAStartup initializes the program for calling WinSock.
+    //The first parameter specifies the highest version of the 
+    //WinSock specification, the program is allowed to use.
+    int wsaret=WSAStartup(0x101,&wsaData);
+
+    //WSAStartup returns zero on success.
+    //If it fails we exit.
+    if(wsaret!=0)
+    {
+        prints ("Fail to init Winsock...");
         return 1;
+    }
+
+    //Now we populate the sockaddr_in structure
+    local.sin_family=AF_INET; //Address family
+    local.sin_addr.s_addr=INADDR_ANY; //Wild card IP address
+    local.sin_port=htons((u_short)20248); //port to use
+
+    //the socket function creates our SOCKET
+    server=socket(AF_INET,SOCK_STREAM,0);
+
+    //If the socket() function fails we exit
+    if(server==INVALID_SOCKET)
+    {
+        prints ("Fail to create SOCKET...");
+        return 0;
+    }
+
+    //bind links the socket we just created with the sockaddr_in 
+    //structure. Basically it connects the socket with 
+    //the local address and a specified port.
+    //If it returns non-zero quit, as this indicates error
+    if(bind(server,(sockaddr*)&local,sizeof(local))!=0)
+    {
+        printf ("Fail to BIND to local host/port...");
+        return 0;
+    }
+
+    //listen instructs the socket to listen for incoming 
+    //connections from clients. The second arg is the backlog
+    if(listen(server,10)!=0)
+    {
+        printf ("Fail on listen ()...");
+        return 0;
+    }
+
+    //we will need variables to hold the client socket.
+    //thus we declare them here.
+    SOCKET client;
+    sockaddr_in from;
+    int fromlen=sizeof(from);
+
+    while(true)//we are looping endlessly
+    {
+        char temp[512];
+
+        //accept() will accept an incoming
+        //client connection
+        client=accept(server, (struct sockaddr*)&from,&fromlen);
+		
+        sprintf(temp,"Your IP is %s\r\n",inet_ntoa(from.sin_addr));
+
+        //we simply send this string to the client
+        send(client,temp,strlen(temp),0);
+        printf ("Connection from %s\r\n", inet_ntoa(from.sin_addr));
+        readprop (40101, 0, 1,85,-1);
+        readprop (50100, 0,1,85,-1);
+
+        //close the client socket
+        closesocket(client);
+
+    }
+
+    //closesocket() closes the socket and releases the socket descriptor
+    closesocket(server);
+
+    //originally this function probably had some use
+    //currently this is just for backward compatibility
+    //but it is safer to call it as I still believe some
+    //implementations use this to terminate use of WS2_32.DLL 
+    WSACleanup();
+
     return 0;
 }
+
 
 
 
